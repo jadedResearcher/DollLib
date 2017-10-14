73b1076127cfa15e8420cb14fae439e75e1e1912
JadedResearcher <gtg287y@gmail.com>
2017-10-14 15:47:18 -0400

    experimenting with a git repo that is just lib files

---
+++ DollRenderer.dart
@@ -0,0 +1,19 @@
+// Copyright (c) 2017, Jenny. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library DollRenderer;
+
+export "src/Dolls/HomestuckDoll.dart";
+export "src/Dolls/HomestuckTrollDoll.dart";
+export "src/Dolls/ConsortDoll.dart";
+export "src/Dolls/DenizenDoll.dart";
+export "src/Dolls/SpriteLayer.dart";
+export "src/Rendering/Renderer.dart";
+export "src/Misc/random.dart";
+export "src/Misc/weighted_lists.dart";
+export "src/Rendering/ReferenceColors.dart";
+
+// TODO: Export any libraries intended for clients of this package.
