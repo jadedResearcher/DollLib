5e29192d6c797fc38d2ba4b77cf52d2463e8f72f
Jenny <gtg287y@gmail.com>
2017-10-16 18:47:39 -0400

    got images out of sync like a dunkass

--- src/Dolls/ConsortDoll.dart
+++ src/Dolls/ConsortDoll.dart
@@ -8,7 +8,7 @@ import "../includes/bytebuilder.dart";
 import "../includes/palette.dart";
 import "../Rendering/ReferenceColors.dart";
 class ConsortDoll extends Doll {
-    int maxBody = 9;
+    int maxBody = 10;
     String folder = "images/Homestuck";
 
     @override
--- src/Dolls/HomestuckDoll.dart
+++ src/Dolls/HomestuckDoll.dart
@@ -12,8 +12,8 @@ class HomestuckDoll extends Doll {
     int renderingType =1;
     @override
     String folder = "images/Homestuck";
-    final int maxBody = 93; //75 is currently colored wrong
-    final int maxHair = 89;
+    final int maxBody = 96; //75 is currently colored wrong
+    final int maxHair = 90;
     final int maxEye =53;
     final int maxMouth = 46;
     final int maxSymbol = 154;
