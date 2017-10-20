c4ec9dbd70bd42bac2480a7382185ba8e2dbf897
Jenny <gtg287y@gmail.com>
2017-10-20 12:58:45 -0400

    text layer knows how to make a text area element

--- src/Dolls/TextLayer.dart
+++ src/Dolls/TextLayer.dart
@@ -1,4 +1,5 @@
 import "../includes/colour.dart";
+import 'dart:html';
 
 /*
 
@@ -9,6 +10,8 @@ It ALSO always has a x/y start location.
 
 class TextLayer {
 
+    Element element;
+    String name;
     String text;
     double topLeftX;
     double topLeftY;
@@ -20,14 +23,27 @@ class TextLayer {
     //ctx.font = "42px Times New Roman";
     String fontName;
 
-    TextLayer(String this.text, double this.topLeftX, double this.topLeftY, {this.emphasis: "", this.maxWidth: 100, this.fontSize: 12, this.fontColor: null, this.fontName: "Times New Roman"}) {
+    TextLayer(String this.name, String this.text, double this.topLeftX, double this.topLeftY, {this.emphasis: "", this.maxWidth: 100, this.fontSize: 12, this.fontColor: null, this.fontName: "Times New Roman"}) {
         if(fontColor == null) {
             fontColor = new Colour(0,0,0);
         }
+
+        element = new DivElement();
+        element.setInnerHtml("$name:");
+        TextAreaElement formElement = new TextAreaElement();
+        formElement.value = text;
+        formElement.onChange.listen((Event e) {
+            print("I felt a change in ${formElement.value}");
+            text = formElement.value;
+        });
+        element.append(formElement);
+
     }
 
     String get font => "$emphasis ${fontSize}px $fontName";
     String get fillStyle => fontColor.toStyleString();
+
+
 
 
 }
