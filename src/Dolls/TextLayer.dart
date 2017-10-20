b4aa52f93999d242928b983be0fd37c3d7d79f14
Jenny <gtg287y@gmail.com>
2017-10-20 10:37:29 -0400

    text layer has emphasis now

--- src/Dolls/TextLayer.dart
+++ src/Dolls/TextLayer.dart
@@ -14,18 +14,19 @@ class TextLayer {
     double topLeftY;
     int maxWidth;
     int fontSize;
+    String emphasis = "";
     //ctx.fillStyle = "#000000";
     Colour fontColor;
     //ctx.font = "42px Times New Roman";
     String fontName;
 
-    TextLayer(String this.text, double this.topLeftX, double this.topLeftY, {this.maxWidth: 100, this.fontSize: 12, this.fontColor: null, this.fontName: "Times New Roman"}) {
+    TextLayer(String this.text, double this.topLeftX, double this.topLeftY, {this.emphasis: "", this.maxWidth: 100, this.fontSize: 12, this.fontColor: null, this.fontName: "Times New Roman"}) {
         if(fontColor == null) {
             fontColor = new Colour(0,0,0);
         }
     }
 
-    String get font => "${fontSize}px $fontName";
+    String get font => "$emphasis ${fontSize}px $fontName";
     String get fillStyle => fontColor.toStyleString();
 
 
