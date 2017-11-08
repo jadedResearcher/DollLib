0e4f0a4daac232f8466eeecb35f0f18144e5a1cf
Jenny <gtg287y@gmail.com>
2017-11-08 09:09:24 -0500

    now need to test char sheet and cards

--- src/Dolls/NamedLayerDoll.dart
+++ src/Dolls/NamedLayerDoll.dart
@@ -15,6 +15,7 @@ abstract class NamedLayerDoll extends Doll {
     //i am assuming type was already read at this point. Type, Exo is required.
     @override
     void initFromReader(ByteReader reader, Palette newP) {
+        initLayers(); //gets body/crown.
         int numFeatures = reader.readExpGolomb();
         print("I think there are ${numFeatures} features");
         int featuresRead = 2; //for exo and doll type
