9ce1ee3d7addea696d3d6aecd0643cd6617ba86c
Jenny <gtg287y@gmail.com>
2017-12-09 18:34:16 -0500

    echelader ready to have chooseable palettes

--- src/CharSheets/Echeladder.dart
+++ src/CharSheets/Echeladder.dart
@@ -12,6 +12,7 @@ import "../loader/loader.dart";
 
     write out text. 16 layers.
     ability to customize all colors in echeladder pallete.
+    font color is inverse of bg color.
  */
 class Echeladder extends CharSheet {
   Echeladder(Doll doll) : super(doll);
@@ -30,7 +31,18 @@ class Echeladder extends CharSheet {
       ..second = '#ba1212'
       ..third = '#ffffee'
       ..fourth = '#f0ff00'
-      ..fifth = '#9c00ff';
+      ..fifth = '#9c00ff'
+      ..six = '#2b6ade'
+      ..seven = '#003614'
+      ..eight = '#f8e69f'
+      ..nine = '#0000ff'
+      ..ten = '#eaeaea'
+      ..eleven = '#ff9600'
+      ..twelve = '#581212'
+      ..thirteen = '#ffa6ac'
+      ..fourteen = '#1f7636'
+      ..fifteen = '#ffe1fc'
+      ..sixteen = '#fcff00';
 
 
   Palette palette = new EcheladderPalette()
@@ -39,7 +51,18 @@ class Echeladder extends CharSheet {
       ..second = '#000000'
       ..third = '#000000'
       ..fourth = '#000000'
-      ..fifth = '#000000';
+      ..fifth = '#000000'
+      ..six = '#000000'
+      ..seven = '#000000'
+      ..eight = '#000000'
+      ..nine = '#000000'
+      ..ten = '#000000'
+      ..eleven = '#000000'
+      ..twelve = '#000000'
+      ..thirteen = '#000000'
+      ..fourteen = '#000000'
+      ..fifteen = '#000000'
+      ..sixteen = '#000000';
 
 
   // TODO: implement barLayers
@@ -89,11 +112,22 @@ class Echeladder extends CharSheet {
 
 class EcheladderPalette extends Palette {
     static String _BORDER = "border";
-    static String _FIRST = "first";
-    static String _SECOND = "second";
-    static String _THIRD = "third";
-    static String _FOURTH = "fourth";
-    static String _FIFTH = "fifth";
+    static String _FIRST = "one";
+    static String _SECOND = "two";
+    static String _THIRD = "three";
+    static String _FOURTH = "four";
+    static String _FIFTH = "five";
+    static String _SIX = "six";
+    static String _SEVEN = "seven";
+    static String _EIGHT = "eight";
+    static String _NINE = "nine";
+    static String _TEN = "ten";
+    static String _ELEVEN = "eleven";
+    static String _TWELVE = "twelve";
+    static String _THIRTEEN = "thirteen";
+    static String _FOURTEEN = "fourteen";
+    static String _FIFTEEN = "fifteen";
+    static String _SIXTEEN = "sixteen";
 
     static Colour _handleInput(Object input) {
         if (input is Colour) {
@@ -140,6 +174,50 @@ class EcheladderPalette extends Palette {
     Colour get fifth => this[_FIFTH];
 
     void set fifth(dynamic c) => this.add(_FIFTH, _handleInput(c), true);
+
+    Colour get six => this[_SIX];
+
+    void set six(dynamic c) => this.add(_SIX, _handleInput(c), true);
+
+    Colour get seven => this[_SEVEN];
+
+    void set seven(dynamic c) => this.add(_SEVEN, _handleInput(c), true);
+
+    Colour get eight => this[_EIGHT];
+
+    void set eight(dynamic c) => this.add(_EIGHT, _handleInput(c), true);
+
+    Colour get nine => this[_NINE];
+
+    void set nine(dynamic c) => this.add(_NINE, _handleInput(c), true);
+
+    Colour get ten => this[_TEN];
+
+    void set ten(dynamic c) => this.add(_TEN, _handleInput(c), true);
+
+    Colour get eleven => this[_ELEVEN];
+
+    void set eleven(dynamic c) => this.add(_ELEVEN, _handleInput(c), true);
+
+    Colour get twelve => this[_TWELVE];
+
+    void set twelve(dynamic c) => this.add(_TWELVE, _handleInput(c), true);
+
+    Colour get thirteen => this[_THIRTEEN];
+
+    void set thirteen(dynamic c) => this.add(_THIRTEEN, _handleInput(c), true);
+
+    Colour get fourteen => this[_FOURTEEN];
+
+    void set fourteen(dynamic c) => this.add(_FOURTEEN, _handleInput(c), true);
+
+    Colour get fifteen => this[_FIFTEEN];
+
+    void set fifteen(dynamic c) => this.add(_FIFTEEN, _handleInput(c), true);
+
+    Colour get sixteen => this[_SIXTEEN];
+
+    void set sixteen(dynamic c) => this.add(_SIXTEEN, _handleInput(c), true);
 
 
 }
